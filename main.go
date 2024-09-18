b6e1f8f3c16d7ea272c3fe3e4457250eecd183e5
Daniel Adinugroho <adinugro@gmail.com>
2024-09-19 06:47:02 +0700

    Refactor code to use env for email destionation

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	sendEmailTo := os.Getenv("SEND_EMAIL_TO")
+
 	today := time.Now().Format("2006-01-02")
 	yesterday := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
 
@@ -36,9 +45,9 @@ func main() {
 	}
 
 	// Send the report via Gmail
-	email := "adinugro@gmail.com"
-	subject := "Daily Stock and Sales Report"
-	body := "Please find the attached stock and sales report."
+	email := sendEmailTo
+	subject := "Laporan Stok dan Penjualan per " + time.Now().Format("2 Jan 2006")
+	body := "Terlampir " + subject
 	err = sendEmailWithGmail(email, subject, body, excelFile)
 	if err != nil {
 		log.Fatalf("Failed to send email: %v", err)
